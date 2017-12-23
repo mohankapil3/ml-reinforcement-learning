59787f8633afbc22a8998939abb8c2a2f232db01
kmo <mohankapil@gmail.com>
2017-12-23 16:33:54 +0000

    added more controls, fixed few bugs

--- game-ui/tic-tac-toe/src/app/app.component.ts
+++ game-ui/tic-tac-toe/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { BoardComponent } from './board/board.component';
 import { Component } from '@angular/core';
 import { RemoteService } from './remote.service';
+import { IState, State } from './board/board.component';
 
 @Component({
   selector: 'app-root',
@@ -17,14 +18,40 @@ export class AppComponent {
   boardDimension = Math.round(Math.sqrt(this.board.size));
   rows = Array.from(Array(this.boardDimension).keys());
   columns = Array.from(Array(this.boardDimension).keys());
+  gameStatus = 'In Play'
 
   boardClicked(position: number): void {
-    this.board.placeMark('O', position)
-    this.remoteService.getNextState(this.board.getCurrentState())
-                      .subscribe(
-                          nextState => this.board.updateState(nextState),
-                          error => console.log('error in invocation - ' + error)
-                      )
+    if (this.board.isPositionEmpty(position) && !this.board.isOver()) {
+      this.board.placeMark('O', position)
+      if (this.board.isOver()) {
+        this.updateGameStatus();
+      } else {
+        this.remoteService
+            .getNextState(this.board.getCurrentState())
+            .subscribe(
+                nextState => this.updateState(nextState),
+                error => console.log('error in invocation - ' + error)
+            )
+      }
+    }
+  }
+
+  startAgain() {
+    this.board.reset();
+    this.gameStatus = 'In Play'
+  }
+
+  private updateState(state: IState) {
+    this.board.updateState(state);
+    this.updateGameStatus();
+  }
+
+  private updateGameStatus() {
+    if (this.board.isWon()) {
+      this.gameStatus = 'Game Finished'
+    } else if (this.board.isDraw()) {
+      this.gameStatus = 'Game Draw'
+    }
   }
 
 }
--- game-ui/tic-tac-toe/src/app/board/board.component.ts
+++ game-ui/tic-tac-toe/src/app/board/board.component.ts
@@ -10,7 +10,7 @@ export class BoardComponent {
 
   size = 9;
 
-  private grid: Array<string> = ["", "", "", "", "", "", "", "", ""];
+  private grid: Array<string> = this.getNewGrid();
 
   placeMark(mark: string, position: number) {
     if (this.isValid(position)) {
@@ -34,11 +34,71 @@ export class BoardComponent {
     return (this.isValidRange(position) && this.isPositionEmpty(position));
   }
 
+  isOver(): boolean {
+    return (this.isWon() || this.isDraw());
+  }
+
+  isDraw(): boolean {
+    return ((!this.isWon()) && (this.isBoardFull()));
+  }
+
+  isWon(): boolean {
+    return (this.checkRows() || this.checkColumns() || this.checkDiagonals());
+  }
+
+  reset() {
+    this.grid = this.getNewGrid();
+  }
+
+  private getNewGrid(): Array<string> {
+    return ["", "", "", "", "", "", "", "", ""];
+  }
+
+  private isBoardFull() {
+    let result = true;
+    for (let i = 0; i < 9; i++) {
+       result = result && !this.isPositionEmpty(i)
+       if (!result) {
+         return false;
+       }
+    }
+    return result;
+  }
+
+  private checkRows(): boolean {
+    for (let i = 0; i < 7; i = i + 3) {
+      if (!this.isPositionEmpty(i) && this.returnMark(i) === this.returnMark(i + 1) && this.returnMark(i + 1) === this.returnMark(i + 2)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private checkColumns(): boolean {
+    for (let i = 0; i < 3; i++) {
+      if (!this.isPositionEmpty(i) && this.returnMark(i) === this.returnMark(i + 3) && this.returnMark(i + 3) === this.returnMark(i + 6)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private checkDiagonals(): boolean {
+    if (!this.isPositionEmpty(0) && this.returnMark(0) === this.returnMark(4) && this.returnMark(4) === this.returnMark(8)) {
+       return true;
+    }
+    if (!this.isPositionEmpty(2) && this.returnMark(2) === this.returnMark(4) && this.returnMark(4) === this.returnMark(6)) {
+       return true;
+    }
+
+    return false;
+  }
+
   private isValidRange(position: number): boolean {
     return (position >= 0 && position <= 8);
   }
 
-  private isPositionEmpty(position: number): boolean {
+  isPositionEmpty(position: number): boolean {
     return (this.grid[position] === undefined || this.grid[position] == "");
   }
 
